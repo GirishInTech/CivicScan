e63fc5bfac4df9b050c772e73d65333339e1d54d
GirishInTech <girish4936@gmail.com>
2025-12-17 20:12:07 +0530

    changes made

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,7 +5,5 @@ from .views import health_check, debug_nav
 urlpatterns = [
     path('', views.home, name='home'),
     path("healthz", health_check, name="healthz"),
-    path('offline/', views.offline, name='offline'),
     path("debug-nav/", debug_nav, name="debug_nav"),
-
 ]
